106a7bfbef42075d9da15ca8931e4cc853ccf55a
KG4SGP <kg4sgp@gmail.com>
2013-06-05 02:04:54 -0400

    Initial commit of simple-rtty. Includes program for producing a RTTY bit stream and a program for FSK.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ simple-rtty
 ===========
 
 A simple RTTY modulator using port audio.
+
+To compile these files:
+
+gcc mod-rtty.c -lm -lportaudio -o mod-rtty
+gcc rtty.c -o rtty
+
+To use these files:
+
+./rtty text-input-file binary-outpu-file
+./mod-rtty binary-output-file audio-output-file
---
+++ rtty.c
@@ -0,0 +1,67 @@
+// Produces an RTTY bit stream for an FSK modulator
+
+#include <stdio.h>
+#include "pwm.h"
+
+int main (int argc[], char* argv[]){
+
+  // open input file
+  FILE* fin;
+  if (strcmp(argv[1], "-") == 0){
+    fin = stdin;
+  } else if( (fin = fopen(argv[1], "rb")) == NULL){
+    printf("Error opening input file...\n");
+    return;
+  }
+
+  // open output file
+  FILE* fout;
+  if (strcmp(argv[2], "-") == 0){
+    fout = stdout;
+  } else if( (fout = fopen(argv[2], "wb")) == NULL){
+    printf("Error opening input file...\n");
+    return;
+  }
+
+  // flush buffers if piped
+  if (fin == stdin) fflush(stdin);
+  if (fout == stdin) fflush(stdout);
+
+  char readbuff, writebuff, shiftToNum = 0;
+  int i = 0;
+  int baudout_length = sizeof(baudout)/sizeof(char);
+
+  while(!feof(fin)){
+    fread(&readbuff, sizeof(readbuff), 1, fin);
+
+    // match 
+    for(i = 0; i < baudout_length; i++){
+      if(readbuff == baudout_letters[i]){
+        if (shiftToNum == 1) {
+          shiftToNum = 0;
+          writebuff = baudout[31];
+          writebuff = (writebuff<<2)+3;
+          fwrite(&writebuff, sizeof(char), 1, fout);
+        }  
+        writebuff = baudout[i];
+        writebuff = (writebuff<<2)+3;
+        fwrite(&writebuff, sizeof(char), 1, fout);
+      }
+
+      if(readbuff != ' ' && readbuff != 10 && readbuff == baudout_figures[i]){
+        if(shiftToNum == 0) {
+          shiftToNum = 1;
+          writebuff = baudout[30];
+          writebuff = (writebuff<<2)+3;
+          fwrite(&writebuff, sizeof(char), 1, fout);
+        }
+        writebuff = baudout[i];
+        writebuff = (writebuff<<2)+3;
+        fwrite(&writebuff, sizeof(char), 1, fout);
+      }
+    }
+
+  }
+
+
+}
